57210748c785c48c1dac26a0628f3ffc7ba1897b
VitaliyPitsa <vitalik.gorshkov.1913@mail.ru>
2022-05-25 22:56:46 +0300

    fix

--- individual2.py
+++ individual2.py
@@ -1,16 +1,12 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import sys
-
 if __name__ == '__main__':
+    line = input("введите предложение ")
 
-   for i in range(105, 999, 7):
-     i1 = i % 10
-     i2 = (i // 10) % 10
-     i3 = (i // 100) % 10
-   if (i1 + i2 + i3) % 7 == 0:
-    print(i)
-
-
+    for ind in range(len(line)-1):
+        if line[ind] == line[ind+1]:
+            print(ind+1, ind+2)
+            break
+    else:
+        print('Таких нет!!!')
